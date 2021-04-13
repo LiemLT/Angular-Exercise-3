5fa1172ec9131023ae7ec34b1598968605a79f48
LiemLT <50836060+LiemLT@users.noreply.github.com>
2021-04-14 00:32:29 +0700

    Add conditions for background-color and color.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from "@angular/core";
 export class AppComponent {
   title = "CodeSandbox";
   showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
